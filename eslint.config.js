9419edf463c8242bc72d4c0add04abbb24df21d1
sayonarvc <vladimir.perminov.99@mail.ru>
2025-12-18 18:34:15 +0500

    t-1: NPM + создание директорий

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'windows'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always']
+    }
+  }
+];
