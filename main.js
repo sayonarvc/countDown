d3a96c28d1fc7addc1a7bd4c6b6f49e295e02416
sayonarvc <vladimir.perminov.99@mail.ru>
2025-12-22 16:06:23 +0500

    t-9

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ import {createElementTime} from './app/domManagers/domManager';
 import {calculateBtn} from './app/fixtures/constans';
 import {logicCalculateTime, updateTimeInterval} from './app/logic/logicCalculateTime';
 
+let updateIntervalId = null;
+
 function showTime() {
   const data = logicCalculateTime();
   createElementTime(data);
 }
 
 function showUpdateTime() {
-  setInterval(updateTimeInterval, 1000);
+  if (updateIntervalId) {
+    clearInterval(updateIntervalId);
+  }
+
+  updateIntervalId = setInterval(updateTimeInterval, 1000);
 }
 
 
